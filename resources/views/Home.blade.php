78c5ba1e4e8f41f481a013347f0f854d1b578a33
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2025-05-11 21:45:19 -0300

    add: Livewire dependecy for frontend

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wiki Japi</title>
+    @livewireStyles
+
+</head>
+<body>
+    <h1>Wiki Japi</h1>
+    <p>Bem-vindo ao Wiki Japi!</p>
+    <p>Esta é uma aplicação de exemplo para demonstrar o uso do Laravel.</p>
+    <p>Você pode começar a explorar as funcionalidades do Laravel a partir daqui.</p>
+
+    @livewireScripts
+</body>
+</html>
