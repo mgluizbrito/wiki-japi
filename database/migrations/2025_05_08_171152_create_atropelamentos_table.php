d981382b24422d8deeadc5014ff00e39cb0d2d4a
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2025-05-19 14:17:45 -0300

    fix: db foreign

--- database/migrations/2025_05_08_171152_create_atropelamentos_table.php
+++ database/migrations/2025_05_08_171152_create_atropelamentos_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->decimal('latitude', 10, 8);
             $table->decimal('longitude', 11, 8);
 
-            $table->foreignId('user_id')->references('id')->on('user');
+            $table->foreignId('user_id')->references('id')->on('users');
         });
     }
 
